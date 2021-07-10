60d474ab23c92f39817b4be54002b1e95ebcf7a4
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-07-10 17:53:43 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx']
+}
