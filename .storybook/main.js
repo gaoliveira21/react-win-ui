f8ab07d709a04a35193670e844f60dbcb754f4a0
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-07-17 14:24:37 -0300

    chore: add @storybook/addon-a11y

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,10 @@
 module.exports = {
-  "stories": [
-    "../src/components/**/*.stories.tsx"
+  stories: [
+    '../src/components/**/*.stories.tsx'
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y'
   ]
 }
