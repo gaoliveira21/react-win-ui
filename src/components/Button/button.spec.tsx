be53780f1696546735e14d556f65719c0a936293
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-07-17 13:24:52 -0300

    refactor: move SUT creation to a factory method

--- src/components/Button/button.spec.tsx
+++ src/components/Button/button.spec.tsx
@@ -1,14 +1,23 @@
 import { render } from '@testing-library/react'
 import { Button } from '.'
 
+type SutTypes = {
+  button: HTMLButtonElement
+}
+
+const makeSut = (buttonContent = 'any_text'): SutTypes => {
+  const { getByRole } = render(<Button>{buttonContent}</Button>)
+
+  const button = getByRole('button') as HTMLButtonElement
+
+  return { button }
+}
+
 describe('Button', () => {
-  it('Should render correct chldren', () => {
-    const buttonContent = 'any_button'
+  it('Should render correct children', () => {
+    const btnChildren = 'any_text'
+    const { button } = makeSut(btnChildren)
 
-    const { getByRole } = render(<Button>{buttonContent}</Button>)
-
-    const button = getByRole('button')
-
-    expect(button.textContent).toBe(buttonContent)
+    expect(button.textContent).toBe(btnChildren)
   })
 })
