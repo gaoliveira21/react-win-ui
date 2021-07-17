e8bd10a76ecbf7990d00fc5e87f2698bef482e6e
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-07-17 13:30:37 -0300

    feat: ensure Button cursor is pointer

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,7 +1,9 @@
+import * as S from './styles'
+
 type ButtonProps = {
   children: React.ReactNode
 }
 
 export function Button({ children }: ButtonProps): React.ReactElement {
-  return <button>{children}</button>
+  return <S.Button>{children}</S.Button>
 }
---
+++ src/components/Button/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+  border: none;
+  background: red;
+  color: #fff;
+  cursor: pointer;
+`
