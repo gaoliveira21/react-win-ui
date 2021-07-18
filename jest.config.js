8e3cfdb4eb23341adb889a884040ac3676444472
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-07-18 15:21:59 -0300

    chore: resolve module alias

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,8 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules/'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/*.stories.tsx'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
+  }
 }
