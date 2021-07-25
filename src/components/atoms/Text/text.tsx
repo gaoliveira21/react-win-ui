a7bcd0ecaf18f5366b2fe5bdc725a104a904db6a
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-07-25 19:54:59 -0300

    feat: ensure Text will render with correct html tag

--- src/components/atoms/Text/text.tsx
+++ src/components/atoms/Text/text.tsx
@@ -2,8 +2,9 @@ import * as S from './styles'
 
 export type TextProps = {
   children: React.ReactNode
+  as?: 'p' | 'strong' | 'span' | 'output' | 'i' | 'small'
 }
 
-export function Text({ children }: TextProps): React.ReactElement {
-  return <S.Text>{children}</S.Text>
+export function Text({ children, as = 'p' }: TextProps): React.ReactElement {
+  return <S.Text as={as}>{children}</S.Text>
 }
