49be686670d7942559231474a81a46574bbf3e0b
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-07-25 20:14:05 -0300

    test: ensure Text will render with correct styles

--- src/components/atoms/Text/text.spec.tsx
+++ src/components/atoms/Text/text.spec.tsx
@@ -12,14 +12,15 @@ type SutTypes = {
 type SutParams = {
   textContent?: string
   as?: TextProps['as']
+  variant?: TextProps['variant']
 }
 
-const makeSut = (
-  params: SutParams = { textContent: 'any_text', as: 'p' }
-): SutTypes => {
+const makeSut = (params: SutParams = { textContent: 'any_text' }): SutTypes => {
   const { container } = render(
     <ThemeProvider>
-      <Text as={params.as}>{params.textContent}</Text>
+      <Text as={params.as} variant={params.variant}>
+        {params.textContent}
+      </Text>
     </ThemeProvider>
   )
 
@@ -30,14 +31,27 @@ const makeSut = (
   }
 }
 
+const assertFontStyle = (sut: Element, font: TextProps['variant'] = 'body') => {
+  expect(sut).toHaveStyleRule(
+    'font-family',
+    bodyFonts[font.toUpperCase()].fontFamily
+  )
+  expect(sut).toHaveStyleRule('font-size', bodyFonts[font.toUpperCase()].size)
+  expect(sut).toHaveStyleRule(
+    'line-height',
+    bodyFonts[font.toUpperCase()].lineHeight
+  )
+  expect(sut).toHaveStyleRule(
+    'font-weight',
+    bodyFonts[font.toUpperCase()].weight
+  )
+}
+
 describe('Testing Text component', () => {
   it('Should render Text with correct initial styles', () => {
     const { sut } = makeSut()
 
-    expect(sut).toHaveStyleRule('font-family', bodyFonts.BODY.fontFamily)
-    expect(sut).toHaveStyleRule('font-size', bodyFonts.BODY.size)
-    expect(sut).toHaveStyleRule('line-height', bodyFonts.BODY.lineHeight)
-    expect(sut).toHaveStyleRule('font-weight', bodyFonts.BODY.weight)
+    assertFontStyle(sut)
   })
 
   it('Should render Text with correct children prop', () => {
@@ -47,16 +61,37 @@ describe('Testing Text component', () => {
     expect(sut.textContent).toBe(textContent)
   })
 
-  it('Should render as paragraph by default', () => {
+  it('Should render Text as paragraph by default', () => {
     const { sut } = makeSut()
 
     expect(sut.tagName.toLowerCase()).toBe('p')
   })
 
-  it('Should render as different html tag if "as" prop has been passed', () => {
+  it('Should render Text with different html tag if "as" prop has been passed', () => {
     const tagName = 'strong'
     const { sut } = makeSut({ as: tagName })
 
     expect(sut.tagName.toLowerCase()).toBe(tagName)
+  })
+
+  it('Should render with "BODY_LARGE" styles if "variants" prop is equal to "body_large"', () => {
+    const variant = 'body_large'
+    const { sut } = makeSut({ variant })
+
+    assertFontStyle(sut, variant)
+  })
+
+  it('Should render with "CAPTION" styles if "variants" prop is equal to "caption"', () => {
+    const variant = 'caption'
+    const { sut } = makeSut({ variant })
+
+    assertFontStyle(sut, variant)
+  })
+
+  it('Should render with "BODY_STRONG" styles if "variants" prop is equal to "body_strong"', () => {
+    const variant = 'body_strong'
+    const { sut } = makeSut({ variant })
+
+    assertFontStyle(sut, variant)
   })
 })
