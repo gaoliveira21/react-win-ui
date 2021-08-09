112ce328301934c970dd4758623b21f0c7f9aa42
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-08-08 21:00:40 -0300

    refactor: add css helper from styled-components

--- src/components/atoms/Text/styles.ts
+++ src/components/atoms/Text/styles.ts
@@ -1,15 +1,13 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 import { TextProps } from '.'
 
 export const Text = styled.p<Pick<TextProps, 'variant' | 'color'>>`
-  font-family: ${({ theme, variant }) =>
-    theme.fonts.text[variant.toUpperCase()].fontFamily};
-  font-size: ${({ theme, variant }) =>
-    theme.fonts.text[variant.toUpperCase()].size};
-  font-weight: ${({ theme, variant }) =>
-    theme.fonts.text[variant.toUpperCase()].weight};
-  line-height: ${({ theme, variant }) =>
-    theme.fonts.text[variant.toUpperCase()].lineHeight};
-  color: ${({ theme, color }) => theme.colors.text[color.toUpperCase()]};
+  ${({ theme, variant, color }) => css`
+    font-family: ${theme.fonts.text[variant.toUpperCase()].fontFamily};}
+    font-size: ${theme.fonts.text[variant.toUpperCase()].size};}
+    font-weight: ${theme.fonts.text[variant.toUpperCase()].weight};}
+    line-height: ${theme.fonts.text[variant.toUpperCase()].lineHeight};}
+    color: ${theme.colors.text[color.toUpperCase()]};
+  `}
 `
