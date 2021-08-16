b6a3f69ede55debcafa3dc7c1a5f943388f94402
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-08-16 15:55:17 -0300

    fix: move declaration file to src

---
+++ src/jest-styled-components.d.ts
@@ -0,0 +1,6 @@
+declare namespace jest {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  interface Matchers<R> {
+    toHaveStyleRule: import('../node_modules/jest-styled-components/src').jest.Matchers['toHaveStyleRule']
+  }
+}
