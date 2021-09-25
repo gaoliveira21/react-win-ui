be8db1f5764e9bb9f027e0dfec0fc334bfb5e228
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-09-25 11:26:28 -0300

    feat(styles): add selection color and change global reset

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -4,6 +4,8 @@ import { ligthTextColors } from '@/styles/colors'
 export const theme = {
   dark: false,
   fonts: {
+    family:
+      "'Segoe UI', --apple-system, BlinkMacSystemFont, Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
     heading: headingFonts,
     text: bodyFonts
   },
