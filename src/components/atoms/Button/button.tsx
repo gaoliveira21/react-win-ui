6d7980cfcfbeba372fab00094660ebedf63420b9
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-09-25 18:57:45 -0300

    feat(button): add Button variant

--- src/components/atoms/Button/button.tsx
+++ src/components/atoms/Button/button.tsx
@@ -4,15 +4,17 @@ export type ButtonProps = {
   children: React.ReactNode
   disabled?: boolean
   onClick?: React.MouseEventHandler<HTMLButtonElement>
+  variant?: 'primary' | 'secondary'
 }
 
 export function Button({
   children,
   disabled = false,
-  onClick
+  onClick,
+  variant = 'primary'
 }: ButtonProps): React.ReactElement {
   return (
-    <S.Button onClick={onClick} disabled={disabled}>
+    <S.Button variant={variant} onClick={onClick} disabled={disabled}>
       {children}
     </S.Button>
   )
--- src/components/atoms/Button/styles.ts
+++ src/components/atoms/Button/styles.ts
@@ -1,17 +1,11 @@
-import styled, { css } from 'styled-components'
+import styled, { css, DefaultTheme } from 'styled-components'
 
-export const Button = styled.button`
-  ${({ theme }) => css`
+import { ButtonProps } from '.'
+
+const modifiers = {
+  primary: (theme: DefaultTheme) => css`
     color: ${theme.colors.text.ON_ACCENT};
-    font-size: ${theme.fonts.text.BODY.size};
-    font-weight: ${theme.fonts.text.BODY.weight};
-    line-height: ${theme.fonts.text.BODY.lineHeight};
-
     background-color: ${theme.colors.fill.accent.ACCENT_PRIMARY};
-    padding: 5px 47px;
-    border-radius: 4px;
-
-    transition: background-color 0.3s;
 
     &:hover {
       background-color: ${theme.colors.fill.accent.ACCENT_SECONDARY};
@@ -22,8 +16,44 @@ export const Button = styled.button`
     }
 
     &:disabled {
-      color: ${theme.colors.text.DISABLED};
+      color: ${theme.colors.text.ON_ACCENT_DISABLED};
       background-color: ${theme.colors.fill.accent.DISABLED};
+    }
+  `,
+
+  secondary: (theme: DefaultTheme) => css`
+    color: ${theme.colors.text.PRIMARY};
+    background-color: ${theme.colors.fill.control.CONTROL_PRIMARY};
+
+    &:hover {
+      background-color: ${theme.colors.fill.control.CONTROL_SECONDARY};
+    }
+
+    &:active {
+      background-color: ${theme.colors.fill.control.CONTROL_TERTIARY};
+    }
+
+    &:disabled {
+      color: ${theme.colors.text.DISABLED};
+      background-color: ${theme.colors.fill.control.CONTROL_PRIMARY};
+    }
+  `
+}
+
+export const Button = styled.button<Pick<ButtonProps, 'variant'>>`
+  ${({ theme, variant }) => css`
+    font-size: ${theme.fonts.text.BODY.size};
+    font-weight: ${theme.fonts.text.BODY.weight};
+    line-height: ${theme.fonts.text.BODY.lineHeight};
+
+    box-shadow: 0 0 2px 2px ${theme.colors.elevation.control.BORDER};
+
+    padding: 5px 47px;
+    border-radius: 4px;
+
+    transition: background-color 0.3s;
+
+    &:disabled {
       cursor: not-allowed;
     }
 
@@ -31,5 +61,7 @@ export const Button = styled.button`
       outline: 3px solid ${theme.colors.stroke.focus.OUTER};
       border: 1px solid ${theme.colors.stroke.focus.INNER};
     }
+
+    ${!!variant && modifiers[variant](theme)};
   `}
 `
