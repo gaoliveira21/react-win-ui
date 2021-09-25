d1a611d2360d6c62b4f6d92d9fd37ec9bac42978
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-09-25 11:14:54 -0300

    fix(text): add correct children args

--- src/components/atoms/Text/__stories__/text.stories.tsx
+++ src/components/atoms/Text/__stories__/text.stories.tsx
@@ -7,26 +7,28 @@ export default {
   component: Text
 } as Meta<TextProps>
 
-const Template: Story<TextProps> = (args) => (
-  <Text {...args}>Caption variant</Text>
-)
+const Template: Story<TextProps> = (args) => <Text {...args}></Text>
 
 export const Caption = Template.bind({}) as typeof Template
 Caption.args = {
-  variant: 'caption'
+  variant: 'caption',
+  children: 'Caption variant'
 }
 
 export const Body = Template.bind({}) as typeof Template
 Body.args = {
-  variant: 'body'
+  variant: 'body',
+  children: 'Body variant'
 }
 
 export const BodyStrong = Template.bind({}) as typeof Template
 BodyStrong.args = {
-  variant: 'body_strong'
+  variant: 'body_strong',
+  children: 'Body Strong variant'
 }
 
 export const BodyLarge = Template.bind({}) as typeof Template
 BodyLarge.args = {
-  variant: 'body_large'
+  variant: 'body_large',
+  children: 'Body Large variant'
 }
