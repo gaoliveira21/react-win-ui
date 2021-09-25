eaba23471a5d93098f8756ab6427602c5b39f3c8
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-09-25 16:28:49 -0300

    feat(button): ensure Button calls onClick

--- src/components/atoms/Button/button.tsx
+++ src/components/atoms/Button/button.tsx
@@ -3,11 +3,17 @@ import * as S from './styles'
 export type ButtonProps = {
   children: React.ReactNode
   disabled?: boolean
+  onClick?: React.MouseEventHandler<HTMLButtonElement>
 }
 
 export function Button({
   children,
-  disabled = false
+  disabled = false,
+  onClick
 }: ButtonProps): React.ReactElement {
-  return <S.Button disabled={disabled}>{children}</S.Button>
+  return (
+    <S.Button onClick={onClick} disabled={disabled}>
+      {children}
+    </S.Button>
+  )
 }
