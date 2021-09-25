25e091ceab0febbef8aa027a777792ab29b3943b
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-09-25 10:33:14 -0300

    docs(text): add Text stories

--- src/components/atoms/Text/__stories__/text.stories.tsx
+++ src/components/atoms/Text/__stories__/text.stories.tsx
@@ -1,12 +1,32 @@
 import { Meta, Story } from '@storybook/react/types-6-0'
 
-import { Text } from '@/components/atoms/Text'
+import { Text, TextProps } from '@/components/atoms/Text'
 
 export default {
   title: 'Text',
   component: Text
-} as Meta
+} as Meta<TextProps>
 
-export const Default: Story = (args) => (
-  <Text {...args}>Basic Text Component</Text>
+const Template: Story<TextProps> = (args) => (
+  <Text {...args}>Caption variant</Text>
 )
+
+export const Caption = Template.bind({}) as typeof Template
+Caption.args = {
+  variant: 'caption'
+}
+
+export const Body = Template.bind({}) as typeof Template
+Body.args = {
+  variant: 'body'
+}
+
+export const BodyStrong = Template.bind({}) as typeof Template
+BodyStrong.args = {
+  variant: 'body_strong'
+}
+
+export const BodyLarge = Template.bind({}) as typeof Template
+BodyLarge.args = {
+  variant: 'body_large'
+}
