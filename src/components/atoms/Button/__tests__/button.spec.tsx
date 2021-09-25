f574f4f3e93bb72a2f07405ff5ab722623e88ad0
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-09-25 16:23:10 -0300

    test(button): ensure Button apply the correct styles

--- src/components/atoms/Button/__tests__/button.spec.tsx
+++ src/components/atoms/Button/__tests__/button.spec.tsx
@@ -2,11 +2,25 @@ import { screen } from '@testing-library/react'
 
 import { Button } from '@/components/atoms/Button'
 import { renderWithTheme } from '@/tests/render-with-theme'
+import { lightAccentFillColors } from '@/styles/colors'
 
 describe('<Button />', () => {
   it('Should render button with correct text content', () => {
     renderWithTheme(<Button>text</Button>)
 
     expect(screen.getByRole('button', { name: /text/i })).toBeInTheDocument()
+  })
+
+  it('Should apply the correct styles if disabled', () => {
+    renderWithTheme(<Button disabled>text</Button>)
+
+    const button = screen.getByRole('button', { name: /text/i })
+
+    expect(button).toBeDisabled()
+    expect(button).toHaveStyleRule(
+      'background-color',
+      lightAccentFillColors.DISABLED,
+      { modifier: ':disabled' }
+    )
   })
 })
