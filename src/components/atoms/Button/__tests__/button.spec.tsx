122c56aad783f2e8d7d322ed7b21446ad1c72d33
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-09-25 19:02:35 -0300

    test(button): add Button variant

--- src/components/atoms/Button/__tests__/button.spec.tsx
+++ src/components/atoms/Button/__tests__/button.spec.tsx
@@ -3,13 +3,20 @@ import userEvent from '@testing-library/user-event'
 
 import { Button } from '@/components/atoms/Button'
 import { renderWithTheme } from '@/tests/render-with-theme'
-import { lightAccentFillColors } from '@/styles/colors'
+import {
+  lightAccentFillColors,
+  lightTextColors,
+  lightControlFillColors
+} from '@/styles/colors'
 
 describe('<Button />', () => {
   it('Should render button with correct text content', () => {
     renderWithTheme(<Button>text</Button>)
 
-    expect(screen.getByRole('button', { name: /text/i })).toBeInTheDocument()
+    const button = screen.getByRole('button', { name: /text/i })
+
+    expect(button).toBeInTheDocument()
+    expect(button).toMatchSnapshot()
   })
 
   it('Should apply the correct styles if disabled', () => {
@@ -32,5 +39,23 @@ describe('<Button />', () => {
     userEvent.click(screen.getByRole('button', { name: /text/i }))
 
     expect(onClick).toHaveBeenCalled()
+  })
+
+  it('Should render a primary Button by default', () => {
+    renderWithTheme(<Button>text</Button>)
+
+    expect(screen.getByRole('button', { name: /text/i })).toHaveStyle({
+      backgroundColor: lightAccentFillColors.ACCENT_PRIMARY,
+      color: lightTextColors.ON_ACCENT
+    })
+  })
+
+  it('Should render a secondary Button', () => {
+    renderWithTheme(<Button variant="secondary">text</Button>)
+
+    expect(screen.getByRole('button', { name: /text/i })).toHaveStyle({
+      backgroundColor: lightControlFillColors.CONTROL_PRIMARY,
+      color: lightTextColors.PRIMARY
+    })
   })
 })
