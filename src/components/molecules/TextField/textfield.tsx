1ee72e72bef2f38b12930a23a71b22ddbecd1697
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-09-26 12:36:58 -0300

    feat(text-field): ensure TextField receive a value and placeholder prop

--- src/components/molecules/TextField/textfield.tsx
+++ src/components/molecules/TextField/textfield.tsx
@@ -5,18 +5,27 @@ import { Label, Input } from '@/components/atoms'
 export type TextFieldProps = {
   label: string
   onChange?: React.ChangeEventHandler<HTMLInputElement>
+  value?: string
+  placeholder?: string
 }
 
 export function TextField({
   label,
-  onChange
+  onChange,
+  value,
+  placeholder
 }: TextFieldProps): React.ReactElement {
   const id = label.replace(/\W/g, '').toLowerCase()
 
   return (
     <S.Wrapper>
       <Label htmlFor={id}>{label}</Label>
-      <Input id={id} placeholder="text" onChange={onChange} />
+      <Input
+        id={id}
+        placeholder={placeholder}
+        onChange={onChange}
+        value={value}
+      />
     </S.Wrapper>
   )
 }
