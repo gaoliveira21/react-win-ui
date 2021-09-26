0dd9eaad52c9ee9b74f3a295ae3dcdd75c7795b3
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-09-26 10:32:04 -0300

    feat(input): add InputProps

--- src/components/atoms/Input/input.tsx
+++ src/components/atoms/Input/input.tsx
@@ -1,5 +1,7 @@
 import * as S from './styles'
 
-export function Input(): React.ReactElement {
-  return <S.Input placeholder="Text" />
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement>
+
+export function Input(props: InputProps): React.ReactElement {
+  return <S.Input {...props} />
 }
