6dc463173a05cff24698e280f557b375662af4f0
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-09-26 14:10:19 -0300

    test(password-field): ensure PasswordField receive a value and placeholder prop

--- src/components/molecules/PasswordField/__tests__/passwordfield.spec.tsx
+++ src/components/molecules/PasswordField/__tests__/passwordfield.spec.tsx
@@ -7,9 +7,11 @@ import { renderWithTheme } from '@/tests/render-with-theme'
 describe('<PasswordField />', () => {
   it('Should calls onChange with correct value', () => {
     const onChange = jest.fn()
-    renderWithTheme(<PasswordField label="text input" onChange={onChange} />)
+    renderWithTheme(
+      <PasswordField label="password input" onChange={onChange} />
+    )
 
-    const input = screen.getByLabelText(/text input/i)
+    const input = screen.getByLabelText(/password input/i)
     const typedValue = 'any_value'
     userEvent.type(input, typedValue)
 
@@ -20,5 +22,22 @@ describe('<PasswordField />', () => {
     expect(event.target.value).toBe(typedValue)
     expect(input).toHaveValue(typedValue)
     expect(input).toMatchSnapshot()
+  })
+
+  it('Should receive a value and placeholder prop', () => {
+    const value = 'any_value'
+    const placeholder = 'any_placeholder'
+    renderWithTheme(
+      <PasswordField
+        label="password input"
+        placeholder={placeholder}
+        value={value}
+        onChange={() => {}}
+      />
+    )
+
+    const input = screen.getByPlaceholderText(placeholder)
+
+    expect(input).toHaveValue(value)
   })
 })
