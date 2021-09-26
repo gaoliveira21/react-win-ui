8eaf63e7d5df7fdffa798a34add237a6c88ac833
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-09-26 11:05:30 -0300

    docs(label): add args

--- src/components/atoms/Label/__stories__/label.stories.tsx
+++ src/components/atoms/Label/__stories__/label.stories.tsx
@@ -1,14 +1,17 @@
 import { Meta, Story } from '@storybook/react'
 
-import { Label } from '@/components/atoms/Label'
+import { Label, LabelProps } from '@/components/atoms/Label'
 import { ThemeProvider } from '@/theme/provider'
 
 export default {
   title: 'Label',
-  component: Label
-} as Meta
+  component: Label,
+  args: {
+    children: 'Helper text'
+  }
+} as Meta<LabelProps>
 
-const Template: Story = (args) => <Label {...args} />
+const Template: Story<LabelProps> = (args) => <Label {...args} />
 
 export const LightDefault = Template.bind({}) as typeof Template
 
