ab886c9cb28d8bd235360b0a4e6012a0cc24efbe
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-09-26 14:16:22 -0300

    feat(password-field): ensure PasswordField hide and show password

--- src/components/molecules/PasswordField/passwordfield.tsx
+++ src/components/molecules/PasswordField/passwordfield.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react'
+import { RiEyeFill } from 'react-icons/ri'
+
 import { Label, Input } from '@/components/atoms'
 
 import * as S from './styles'
@@ -16,6 +19,9 @@ export function PasswordField({
   placeholder
 }: PasswordFieldProps): React.ReactElement {
   const id = label.replace(/\W/g, '').toLowerCase()
+  const [showPassword, setShowPassword] = useState(false)
+
+  const togglePassword = () => setShowPassword((prevValue) => !prevValue)
 
   return (
     <S.Wrapper>
@@ -25,7 +31,14 @@ export function PasswordField({
         placeholder={placeholder}
         onChange={onChange}
         value={value}
+        type={showPassword ? 'text' : 'password'}
       />
+      <S.TogglePasswordButton
+        aria-label="toggle password"
+        onClick={togglePassword}
+      >
+        <RiEyeFill />
+      </S.TogglePasswordButton>
     </S.Wrapper>
   )
 }
