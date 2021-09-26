679e6f6fab0e3ad6af48b6a33c8aa0cf7727e23f
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-09-26 13:29:37 -0300

    feat(text-field): ensure TextField render ClearButton correctly

--- src/components/molecules/TextField/styles.ts
+++ src/components/molecules/TextField/styles.ts
@@ -1,10 +1,37 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const Wrapper = styled.div`
   display: inline-flex;
   flex-direction: column;
+  position: relative;
 
   > label {
     margin-bottom: 8px;
   }
+`
+
+export const ClearButton = styled.button`
+  ${({ theme }) => css`
+    position: absolute;
+    bottom: 3px;
+    right: 8px;
+    color: ${theme.colors.text.SECONDARY};
+    background-color: ${theme.colors.fill.subtle.TRANSPARENT};
+    padding: 6px 8px;
+    font-size: 12px;
+    border-radius: 4px;
+    transition: background-color 0.3s;
+
+    &:hover {
+      background-color: ${theme.colors.fill.subtle.SECONDARY};
+    }
+
+    &:active {
+      background-color: ${theme.colors.fill.subtle.TERTIARY};
+    }
+
+    &:focus {
+      outline: 1px solid ${theme.colors.stroke.focus.OUTER};
+    }
+  `}
 `
--- src/components/molecules/TextField/textfield.tsx
+++ src/components/molecules/TextField/textfield.tsx
@@ -1,6 +1,8 @@
-import * as S from './styles'
+import { useState } from 'react'
 
 import { Label, Input } from '@/components/atoms'
+
+import * as S from './styles'
 
 export type TextFieldProps = {
   label: string
@@ -16,16 +18,28 @@ export function TextField({
   placeholder
 }: TextFieldProps): React.ReactElement {
   const id = label.replace(/\W/g, '').toLowerCase()
+  const [showClear, setShowClear] = useState(false)
+  let timerId: NodeJS.Timeout
+
+  const onBlur = () => {
+    timerId = setTimeout(() => setShowClear(false))
+  }
+
+  const onFocus = () => {
+    clearTimeout(timerId)
+  }
 
   return (
-    <S.Wrapper>
+    <S.Wrapper onBlur={onBlur} onFocus={onFocus}>
       <Label htmlFor={id}>{label}</Label>
       <Input
         id={id}
         placeholder={placeholder}
         onChange={onChange}
         value={value}
+        onClick={() => setShowClear(true)}
       />
+      {showClear && <S.ClearButton aria-label="clear">X</S.ClearButton>}
     </S.Wrapper>
   )
 }
